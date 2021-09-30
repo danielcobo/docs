6467acd576a2214f3f515833fec28d9a494efab7
danielcobo <danielcobo@users.noreply.github.com>
2021-09-30 21:55:39 +0200

    Make readFiles.js more modular

--- src/readFiles.js
+++ src/readFiles.js
@@ -2,11 +2,19 @@ const readPaths = require('./readPaths.js');
 const fs = require('@danielcobo/fs');
 
 /**
- * Read relevant files
+ * @typedef {Object} RawFile
  * @private
+ * @property {string} path - filepath
+ * @property {string} content - content string
  */
-const readFiles = async function readFiles() {
-  const paths = await readPaths();
+
+/**
+ * Read files and return an array of RawFile objects
+ * @private
+ * @param {Array.String} paths
+ * @returns {RawFile}
+ */
+const readFiles = async function readFiles(paths) {
   const contents = await Promise.all(
     paths.map(function (filepath) {
       return fs.read(filepath);
