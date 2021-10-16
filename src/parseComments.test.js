2e4302a6de213bfa2e2cd61d83fdda0cd3a7c60b
danielcobo <danielcobo@users.noreply.github.com>
2021-10-16 14:48:10 +0200

    Update parseComments tests

--- src/parseComments.test.js
+++ src/parseComments.test.js
@@ -1,6 +1,7 @@
 const parseComments = require('./parseComments.js');
+const path = require('path');
 
-test('Test parseComments.js - description', function () {
+test('Test parseComments.js - description', async function () {
   const content = `
   /**
    * Function description
@@ -8,13 +9,16 @@ test('Test parseComments.js - description', function () {
    * @param {number} num - a number
    */
   `;
-  const comments = parseComments([
-    { path: './every/path/starts/with/a/single/step', content: content },
+  const comments = await parseComments([
+    {
+      content: content,
+      path: './every/path/starts/with/a/single/step',
+    },
   ]);
   expect(comments[0].description).toStrictEqual('Function description');
 });
 
-test('Test parseComments.js - description by typedef', function () {
+test('Test parseComments.js - description by typedef', async function () {
   const content = `
   /**
    * @typedef Book - a book type
@@ -22,13 +26,13 @@ test('Test parseComments.js - description by typedef', function () {
    * @param {number} pages - number of pages
    */
   `;
-  const comments = parseComments([
+  const comments = await parseComments([
     { path: './every/path/starts/with/a/single/step', content: content },
   ]);
   expect(comments[0].description).toStrictEqual('A book type');
 });
 
-test('Test parseComments.js - source', function () {
+test('Test parseComments.js - source', async function () {
   const content = `
   /**
    * Function description
@@ -36,16 +40,20 @@ test('Test parseComments.js - source', function () {
    * @param {number} num - a number
    */
   `;
-  const comments = parseComments([
-    { path: './every/path/starts/with/a/single/step', content: content },
+  const comments = await parseComments([
+    {
+      content: content,
+      path: './every/path/starts/with/a/single/step',
+    },
   ]);
   expect(comments[0].source).toStrictEqual({
     path: './every/path/starts/with/a/single/step',
+    url: 'every/path/starts/with/a/single/step',
     line: 2,
   });
 });
 
-test('Test parseComments.js - properties', function () {
+test('Test parseComments.js - properties', async function () {
   const content = `
   /**
    * @typedef {Object} Source
@@ -53,7 +61,7 @@ test('Test parseComments.js - properties', function () {
    * @property {number} lnnumber - source line number
    */
   `;
-  const comments = parseComments([
+  const comments = await parseComments([
     { path: './every/path/starts/with/a/single/step', content: content },
   ]);
   expect(comments[0].property).toStrictEqual([
@@ -66,7 +74,7 @@ test('Test parseComments.js - properties', function () {
   ]);
 });
 
-test('Test parseComments.js - returns', function () {
+test('Test parseComments.js - returns', async function () {
   const content = `
   /**
    * Function description
@@ -75,7 +83,7 @@ test('Test parseComments.js - returns', function () {
    * @returns {number} - number of numbers
    */
   `;
-  const comments = parseComments([
+  const comments = await parseComments([
     { path: './every/path/starts/with/a/single/step', content: content },
   ]);
   expect(comments[0].returns).toStrictEqual({
@@ -84,7 +92,7 @@ test('Test parseComments.js - returns', function () {
   });
 });
 
-test('Test parseComments.js - alias', function () {
+test('Test parseComments.js - alias', async function () {
   const content = `
   /**
    * Function description
@@ -94,13 +102,13 @@ test('Test parseComments.js - alias', function () {
    * @returns {number} - number of numbers
    */
   `;
-  const comments = parseComments([
+  const comments = await parseComments([
     { path: './every/path/starts/with/a/single/step', content: content },
   ]);
-  expect(comments[0].name).toStrictEqual('IceCream()');
+  expect(comments[0].name).toStrictEqual('.IceCream()');
 });
 
-test('Test parseComments.js - Alias', function () {
+test('Test parseComments.js - Alias', async function () {
   const content = `
   /**
    * Function description
@@ -110,13 +118,16 @@ test('Test parseComments.js - Alias', function () {
    * @returns {number} - number of numbers
    */
   `;
-  const comments = parseComments([
-    { path: './every/path/starts/with/a/single/step', content: content },
+  const comments = await parseComments([
+    {
+      content: content,
+      path: './every/path/starts/with/a/single/step',
+    },
   ]);
-  expect(comments[0].name).toStrictEqual('IceCream()');
+  expect(comments[0].name).toStrictEqual('.IceCream()');
 });
 
-test('Test parseComments.js - package name', function () {
+test('Test parseComments.js - package name', async function () {
   const content = `
   /**
    * Function description
@@ -125,14 +136,14 @@ test('Test parseComments.js - package name', function () {
    * @returns {number} - number of numbers
    */
   `;
-  const comments = parseComments(
+  const comments = await parseComments(
     [{ path: './index.js', content: content }],
     'MyPackage'
   );
   expect(comments[0].name).toStrictEqual('MyPackage()');
 });
 
-test('Test parseComments.js - 1 implied name max.', function () {
+test('Test parseComments.js - 1 implied name max.', async function () {
   const content = `
   /**
    * Function description
@@ -148,8 +159,13 @@ test('Test parseComments.js - 1 implied name max.', function () {
     * @returns {number} - number of numbers
     */
   `;
-  expect(() => parseComments([{ path: './index.js', content: content }]))
-    .toThrow(`
+  let errMessage;
+  await parseComments([{ path: './index.js', content: content }]).catch(
+    function (err) {
+      errMessage = err.message;
+    }
+  );
+  expect(errMessage).toStrictEqual(`
 There can only be 1 implied name per file.
 Already implied:
 File: ./index.js
